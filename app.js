322c40bff0996b066d5e6cecdfb5553d312dba08
Gokul Simson <gokulsimson@glorysoftech.com>
2024-06-20 10:41:15 +0530

    Profile Image Update firestore

--- app.js
+++ app.js
@@ -1,51 +1,89 @@
 import { 
-    getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword, sendEmailVerification,
+    getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, 
+    signInWithEmailAndPassword, sendEmailVerification,
     sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup
 } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 
-import { getFirestore, doc, setDoc  } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
+
+import { getStorage, getDownloadURL, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-storage.js";
 
 const auth = getAuth();
 const db = getFirestore();
+const storage = getStorage();
 
+let file = null;
+
+const UI_image_FileInput = document.getElementById("image-file-input");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const signUpBtn = document.getElementById("signup-btn");
 const UI_error_msg = document.getElementById("error-msg");
 const UI_signup_form = document.getElementById("signup-form");
+
 const UI_user_profile = document.getElementById("user-profile");
 const UI_user_email = document.getElementById("user-email");
 const UI_main_view = document.getElementById("main-view");
 const UI_logout_btn = document.getElementById("logout-btn");
+
 const UI_login_form = document.getElementById("login-form");
 const UI_login_email = document.getElementById("login-email");
 const UI_login_password = document.getElementById("login-password");
 const UI_login_btn = document.getElementById("login-btn");
 const UI_need_an_account_btn = document.getElementById("need-an-account-btn");
 const UI_have_an_account_btn = document.getElementById("have-an-account-btn");
+
 const UI_emailverification_view = document.getElementById("email-verification");
 const UI_verification_resend_btn = document.getElementById("verification-resend-btn");
+const UI_email_verification_logout = document.getElementById("email-verification-logout");
+
 const UI_forgot_password_btn = document.getElementById("forgot-password-btn");
 const UI_reset_password_form = document.getElementById("reset-password-form");
 const UI_reset_password_btn = document.getElementById("reset-password-btn");
 const UI_reset_password_email = document.getElementById("reset-password-email");
 const UI_reset_msg = document.getElementById("reset-msg");
+
 const UI_login_with_google_btn = document.getElementById("login-with-google-btn");
 const UI_signup_user_name = document.getElementById("name");
 const UI_signup_user_phone = document.getElementById("phone");
 
-onAuthStateChanged(auth, (user) =>{
-    console.log(user);
+const UI_update_name = document.getElementById("update-name");
+const UI_update_phone = document.getElementById("update-phone");
+const UI_update_email = document.getElementById("update-email");
+const UI_update_btn = document.getElementById("update-btn");
+const UI_update_msg = document.getElementById("update-msg");
+const UI_profile_picture_img = document.getElementById("profile-picture-img");
+const UI_profile_update_img_input = document.getElementById("update-image-file-input");
+
+onAuthStateChanged(auth, async (user) =>{
     if(user && user.emailVerified){
         UI_login_form.style.display = "none";
         UI_user_profile.style.display = "block";
         UI_emailverification_view.style.display = "none";
         UI_user_email.innerHTML = user.email;
+        const docRef = doc(db, "users", user.uid);
+        try {
+            const docSnap = await getDoc(docRef);
+            console.log(docSnap.data());
+            UI_update_name.value = docSnap.data().name;
+            UI_update_email.value = docSnap.data().email;
+            UI_update_phone.value = docSnap.data().phone_number;
+            
+            const fileRef = ref(storage, `user_profile_image/${user.uid}/${user.uid}-profile-image`);
+                const profiel_imag_url = await getDownloadURL(fileRef);
+                console.log(profiel_imag_url);
+            UI_profile_picture_img.src=profiel_imag_url;
+        } catch (error) {
+            console.log(error);
+        }
     }
     else if(user){
         UI_login_form.style.display = "none";
         UI_user_profile.style.display = "none";
         UI_emailverification_view.style.display = "block";
+        const fileRef = ref(storage, `user_profile_image/${user.uid}/${user.uid}-profile-image`);
+        const profiel_imag_url = await getDownloadURL(fileRef);
+        console.log(profiel_imag_url);
     }
     else{
         UI_login_form.style.display = "block";
@@ -74,6 +112,8 @@ const singnUpClicked = async (e)=>{
             UI_emailverification_view.style.display = "block";
         }
         UI_user_email.innerHTML = userCredential.user.email;
+        
+        //Default email verification
         await sendEmailVerification(userCredential.user);
 
         const docRef = doc(db, "users", userCredential.user.uid);
@@ -82,6 +122,9 @@ const singnUpClicked = async (e)=>{
             phone_number:UI_signup_user_phone.value,
             email:userCredential.user.email
         });
+        
+        //upload profile image
+        uploadImage(userCredential.user.uid);
         
     }catch(error){
         console.log(error);
@@ -93,6 +136,8 @@ const singnUpClicked = async (e)=>{
 const logOutClicked = async () => {
     try{
         await signOut(auth);
+        UI_update_msg.classList.add("hidden");
+        UI_emailverification_view.style.display = "none";
         email.value="";
         password.value="";
     }
@@ -158,6 +203,58 @@ const loginWithGoogleClicked = async (e) => {
     }
 }
 
+const updateClicked = async (e) => {
+    e.preventDefault();
+    UI_main_view.classList.add("loading");
+    const docRef = doc(db, "users", auth.currentUser.uid);
+    try {
+        await setDoc(docRef, {
+            name: UI_update_name.value,
+            phone_number:UI_update_phone.value,
+            email:UI_update_email.value
+        }, {merge:true});
+
+        if(file!=null){
+            uploadImage(auth.currentUser.uid);
+        }
+
+        UI_update_msg.classList.remove("hidden");
+        UI_update_msg.innerHTML = "Updated successfully";
+        UI_update_msg.classList.add("success");
+        
+    } catch (error) {
+        console.log(error);
+    }
+    UI_main_view.classList.remove("loading");
+}
+
+const selectProfileImage = async (e) => {
+    file = e.target.files[0];
+}
+
+const uploadImage = async (uid) => {
+    if(file != null){
+        const storageRef = ref(storage, `user_profile_image/${uid}/${uid}-profile-image`);
+        try {
+            await uploadBytes(storageRef, file)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const updateImageFileChosen = (e) => {
+    if(e.target.files[0]!=null){
+        file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = (e) => {
+            console.log(e.target.result);
+            UI_profile_picture_img.src = e.target.result;
+        }
+    }    
+}
+
 const hideAll = () => {
     UI_login_form.style.display = "none";
     UI_signup_form.style.display ="none";
@@ -175,6 +272,10 @@ UI_verification_resend_btn.addEventListener("click",resendVerificationClicked);
 UI_forgot_password_btn.addEventListener("click",forgotPasswordClicked);
 UI_reset_password_btn.addEventListener("click",resetPasswordClicked);
 UI_login_with_google_btn.addEventListener("click",loginWithGoogleClicked);
+UI_update_btn.addEventListener("click",updateClicked);
+UI_email_verification_logout.addEventListener("click", logOutClicked);
+UI_image_FileInput.addEventListener("change", selectProfileImage);
+UI_profile_update_img_input.addEventListener("change", updateImageFileChosen);
 
 const formateErrorMsg=(error_code, mode)=>{
     if(mode==="register"){
@@ -196,6 +297,5 @@ const formateErrorMsg=(error_code, mode)=>{
             return "This email already registered";
         else
             return error_code
-    }
-    
+    }    
 }
